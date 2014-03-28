4d71af51b553dc32dd45b628937e32db3ca4b0a5
Alvin Chan <aschan@gmail.com>
2014-03-28 16:34:54 -0700

    Check active page before running front-end JS

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -9,39 +9,41 @@
     }
   }
 
-  addEvent(document.getElementById('battle'), 'click', function(e) {
-    if (e.target.tagName === 'A') {
-      e.preventDefault();
+  if (document.getElementById('battle')) {
+    addEvent(document.getElementById('battle'), 'click', function(e) {
+      if (e.target.tagName === 'A') {
+        e.preventDefault();
 
-      var vote = e.target,
-        http = new XMLHttpRequest();
+        var vote = e.target,
+          http = new XMLHttpRequest();
 
-      http.onreadystatechange = function() {
-        if (http.readyState === 4 && http.status === 200) {
-          var choices = vote.parentNode.parentNode.childNodes,
-            votes = JSON.parse(http.responseText);
+        http.onreadystatechange = function() {
+          if (http.readyState === 4 && http.status === 200) {
+            var choices = vote.parentNode.parentNode.childNodes,
+              votes = JSON.parse(http.responseText);
 
-          /* Clear 'selected' class from all choices and update vote counts */
-          for (var i = 0; i < choices.length; i++) {
-            /* Remove the 'selected' class */
-            choices[i].classList.remove('selected');
+            /* Remove the 'selected' class and update the vote counts for each choice */
+            for (var i = 0; i < choices.length; i++) {
+              /* Remove the 'selected' class */
+              choices[i].classList.remove('selected');
 
-            /* Update the count */
-            for (var j = 0; j < votes.length; j++) {
-              if (choices[i].childNodes[0].href.match(new RegExp(votes[j].choice + '$'))) {
-                choices[i].childNodes[0].childNodes[1].innerHTML = votes[j].count;
-                continue;
+              /* Update the vote count */
+              for (var j = 0; j < votes.length; j++) {
+                if (choices[i].childNodes[0].href.match(new RegExp(votes[j].choice + '$'))) {
+                  choices[i].childNodes[0].childNodes[1].innerHTML = votes[j].count;
+                  continue;
+                }
               }
             }
+
+            /* Add the 'selected' class to the choice that was picked */
+            vote.parentNode.classList.add('selected');
           }
+        };
 
-          /* Add 'selected' class to the choice that was picked */
-          vote.parentNode.classList.add('selected');
-        }
-      };
-
-      http.open('GET', vote.href, true);
-      http.send();
-    }
-  });
+        http.open('GET', vote.href, true);
+        http.send();
+      }
+    });
+  }
 }());
